94cbe007fb8c1a8a2142731c52cea223b637d55c
Your Name <you@example.com>
2024-03-25 17:14:28 +0200

    Unnecessary comment removed

--- backend/TaxCalculator.Module/Controllers/TaxCalculationController.cs
+++ backend/TaxCalculator.Module/Controllers/TaxCalculationController.cs
@@ -38,27 +38,5 @@ public class TaxCalculationController : ApiControllerBase
         var command = Mapper.Map<CalculateTaxCommand>(taxCalculationDto);
         var result = await ServiceBus.SendAsync<CalculateTaxCommand, OutgoingTaxCalculationDTO>(command, cancellationToken);
         return Send(result);
-
-
-        //var grossSalary = taxCalculationRequest.GrossSalary;
-        //var taxBands = await _context.TaxBands.OrderBy(b => b.LowerLimit).ToListAsync();
-        //var taxCalculationResult = new TaxCalculationResult { GrossAnnualSalary = grossSalary };
-        //decimal totalTax = 0;
-
-        //foreach (var band in taxBands)
-        //{
-        //    if (grossSalary > band.LowerLimit)
-        //    {
-        //        var taxableAmountInBand = band.UpperLimit.HasValue && grossSalary > band.UpperLimit ? band.UpperLimit.Value - band.LowerLimit : grossSalary - band.LowerLimit;
-        //        var taxInBand = taxableAmountInBand * (band.TaxRate / 100);
-        //        totalTax += taxInBand;
-        //        if (band.UpperLimit.HasValue && grossSalary <= band.UpperLimit) break;
-        //    }
-        //}
-
-        //taxCalculationResult.AnnualTaxPaid = totalTax;
-        //taxCalculationResult.NetAnnualSalary = grossSalary - totalTax;
-
-        //return Ok(taxCalculationResult);
     }
 }
